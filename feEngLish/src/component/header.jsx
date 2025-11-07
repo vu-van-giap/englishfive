477d320ca04836810705e6a414d7d7aa96b928bf
VanDung <trvdung2003@gmail.com>
2025-11-07 20:31:00 +0700

    thaydoilink

--- feEngLish/src/component/header.jsx
+++ feEngLish/src/component/header.jsx
@@ -118,29 +118,29 @@ export default function Header() {
                   <div class="mt-6 flow-root">
                     <div class="-my-6 divide-y divide-gray-500/10">
                       <div class="space-y-2 py-6">
+                        <Link
+                          to="/"
+                          class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                        >
+                          Home
+                        </Link>
                         <a
                           href="#"
                           class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                         >
-                          Product
+                          FlashCard
                         </a>
                         <a
                           href="#"
                           class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                         >
-                          Features
+                          Quiz
                         </a>
                         <a
                           href="#"
                           class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                         >
-                          Marketplace
-                        </a>
-                        <a
-                          href="#"
-                          class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                        >
-                          Company
+                          Audio
                         </a>
                       </div>
                       <div class="py-6">
@@ -150,12 +150,12 @@ export default function Header() {
                         >
                           Log in
                         </a>
-                        <a
-                          href="#"
+                        <Link
+                          to="/register"
                           class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                         >
-                          Register
-                        </a>
+                          Sign up
+                        </Link>
                       </div>
                     </div>
                   </div>
